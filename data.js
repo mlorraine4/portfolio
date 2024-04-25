267be70314aa205f73b05fc193f9451cc6bfc8bb
Maria Silvia <maria.lorraine.silvia4@gmail.com>
2024-04-25 13:54:28 +0000

    style: adds preview photos, adds content to detail pages

---
+++ data.js
@@ -0,0 +1,44 @@
+// inventory app (meow meadows), members only, blog api (garden of pages), messaging app (strife)
+
+const highlightedProjects = [
+  {
+    name: "Fumblr",
+    tech: ["Node.js", "MongoDB", "JS", "HTML", "CSS", "Firebase"],
+    description: "",
+    preview_url: "",
+    live_demo: "",
+  },
+  {
+    name: "Battleship",
+    tech: ["JS", "HTML", "CSS"],
+    description: "",
+    preview_url: "",
+    live_demo: "https://mlorraine4.github.io/battleship/",
+  },
+  {
+    name: "Garden of Pages",
+    tech: ["JS", "HTML", "CSS"],
+    description: "",
+    preview_url: "",
+    live_demo: "https://mlorraine4.github.io/battleship/",
+  },
+  {
+    name: "Strife",
+    tech: ["JS", "Pug", "CSS", "Node.js", "Mongoose or MongoDB"],
+    TOP_description:
+      "The goal of this project is to make your own version of an app that allows users to send/recieve messages. ",
+    dependencies: "",
+    description: "",
+    preview_url: "",
+    live_demo: "https://mlorraine4.github.io/battleship/",
+    projectGoals: "",
+    issues: "",
+  },
+];
+
+const otherProjects = [
+  // {
+  //   name: "Meow Meadows",
+  //   live_demo: "https://meowmeadows.mariasilvia.me/",
+  // },
+];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
+router.get("/", function (req, res, next) {
+  res.render("home");
+});
+
+router.get("/projects/fumblr", function (req, res, next) {
+  res.render("fumblr-detail");
+});
+
+router.get("/projects/glass-gardens", function (req, res, next) {
+  res.render("glassgardens-detail");
+});
+
+router.get("/projects/battleship", function (req, res, next) {
+  res.render("battleship-detail");
 });
 
 module.exports = router;
